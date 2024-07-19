bbb3dc18aa71038f71974c75e539d0a89b9f0255
Murali Krishna <contact.r.murali@gmail.com>
2024-07-19 15:27:42 +0530

    page updated

--- frontend/src/components/user/allBooks.js
+++ frontend/src/components/user/allBooks.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { SearchIcon, HeartIcon } from '@heroicons/react/outline';
 import { HeartIcon as HeartIconSolid } from '@heroicons/react/solid';
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import NavBar from '../navbar';
 import Footer from '../footer';
-import {Link} from 'react-router-dom';
+
 
 export const booksData = [
   { id: 1, title: 'To Kill a Mockingbird', imageUrl: 'https://images.gr-assets.com/books/1553383690l/2657.jpg' },
@@ -46,6 +48,15 @@ const AllBooks = ({bookmarkedBooks, toggleBookmark}) => {
   // Change page
   const paginate = pageNumber => setCurrentPage(pageNumber);
 
+  const shareBook = (bookId) => {
+    const bookUrl = `${window.location.origin}/books/${bookId}`;
+    navigator.clipboard.writeText(bookUrl).then(() => {
+      toast.success('Book URL copied to clipboard!');
+    }).catch(err => {
+      toast.error('Failed to copy the text: ', err);
+    });
+  };
+
   return (
     <div>
       <NavBar />
@@ -82,14 +93,19 @@ const AllBooks = ({bookmarkedBooks, toggleBookmark}) => {
                     <HeartIcon className="h-6 w-6 text-gray-500" />
                   )}
                 </div>
+                <div
+                  className="absolute top-10 right-2 bg-white rounded-full p-1 cursor-pointer"
+                  onClick={() => shareBook(book.id)}
+                >
+                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z"/></svg>
+                </div>
+                <ToastContainer />
               </div>
               <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2 text-center">{book.title}</div>
-                <div className="flex justify-between">
-                  <Link to="/display-books/rating" className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition-colors duration-300">
-                    Rating
-                  </Link>
-                  <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded transition-colors duration-300">
+                <div className="flex">
+                  
+                  <button className="bg-gray-300 ml-24 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded transition-colors duration-300">
                     View Book
                   </button>
                 </div>
@@ -97,6 +113,10 @@ const AllBooks = ({bookmarkedBooks, toggleBookmark}) => {
             </div>
           ))}
         </div>
+
+        {/* share option */}
+
+
 
         {/* Pagination */}
         <div className="mt-6">
