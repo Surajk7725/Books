7c8ce6c9b9cbe3b506c799abaf2fd626324bdde4
Suraj Kumar <surajkumar.ayanokoji.7@gmail.com>
2024-09-09 19:00:28 +0530

    Done some more changes

--- backend/server.js
+++ backend/server.js
@@ -54,4 +54,6 @@ app.use('/api/contact',contactRoutes);
 
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT,()=> console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
